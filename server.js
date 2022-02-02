be9d85ccec7b9a6512dfefa6862f36d60acd558b
Chisha A <chisha.amaechi@tortoisemedia.com>
2022-02-02 22:53:50 +0000

    Updating index.ejs file so there are no HTML files in project #5

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const blogRouter = require("./routes/blogs");
 const server = express();
 
 server.set("view engine", "ejs");
+
+
+server.use("/blogs", blogRouter)
 
 server.get("/", (request, response) => {
     //response.send("Im here for you");
